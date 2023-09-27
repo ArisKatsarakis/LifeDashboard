028fd921843d9ff9d0e8737b21a0b721d19795db
KatsarakisAris <ariskatsarakis@hotmail.com>
2023-09-27 18:59:13 +0300

    Spring Boot Project added

---
+++ life-dashboard/src/main/java/gr/ariskatsarakis/lifedashboard/LifeDashboardApplication.java
@@ -0,0 +1,13 @@
+package gr.ariskatsarakis.lifedashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeDashboardApplication.class, args);
+    }
+
+}
---
+++ life-dashboard/src/test/java/gr/ariskatsarakis/lifedashboard/LifeDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.ariskatsarakis.lifedashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

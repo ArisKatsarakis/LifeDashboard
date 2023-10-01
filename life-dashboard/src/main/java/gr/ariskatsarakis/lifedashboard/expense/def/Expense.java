da009138bbe4d8f42b70a8a005f454ed1eb3444e
KatsarakisAris <ariskatsarakis@hotmail.com>
2023-10-01 14:01:25 +0300

    Add new fields to Expense class.

--- life-dashboard/src/main/java/gr/ariskatsarakis/lifedashboard/expense/def/Expense.java
+++ life-dashboard/src/main/java/gr/ariskatsarakis/lifedashboard/expense/def/Expense.java
@@ -1,6 +1,10 @@
 package gr.ariskatsarakis.lifedashboard.expense.def;
 
+import gr.ariskatsarakis.lifedashboard.expense.beans.ExpenseType;
 import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Table( name = "life_expenses")
@@ -18,12 +22,18 @@ public class Expense {
     private long expenseId;
     private String expenseName;
 
-    public Expense( String expenseName) {
+    private ExpenseType expenseType;
+
+    private LocalDate dateCreated;
+
+    private BigDecimal moneySpent;
+
+
+    public Expense(String expenseName, ExpenseType expenseType, LocalDate dateCreated, BigDecimal moneySpent) {
         this.expenseName = expenseName;
-    }
-
-    public Expense() {
-
+        this.expenseType = expenseType;
+        this.dateCreated = dateCreated;
+        this.moneySpent = moneySpent;
     }
 
     @Override
@@ -31,8 +41,16 @@ public class Expense {
         return "Expense{" +
                 "expenseId=" + expenseId +
                 ", expenseName='" + expenseName + '\'' +
+                ", expenseType=" + expenseType +
+                ", dateCreated=" + dateCreated +
+                ", moneySpent=" + moneySpent +
                 '}';
     }
+
+    public Expense() {
+
+    }
+
 
     public long getExpenseId() {
         return expenseId;
@@ -48,5 +66,29 @@ public class Expense {
 
     public void setExpenseName(String expenseName) {
         this.expenseName = expenseName;
+    }
+
+    public ExpenseType getExpenseType() {
+        return expenseType;
+    }
+
+    public void setExpenseType(ExpenseType expenseType) {
+        this.expenseType = expenseType;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public BigDecimal getMoneySpent() {
+        return moneySpent;
+    }
+
+    public void setMoneySpent(BigDecimal moneySpent) {
+        this.moneySpent = moneySpent;
     }
 }

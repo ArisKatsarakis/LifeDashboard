ee4b4621f990d64ee87b1e60ac8bed0da3aee517
ArisKatsarakis <ariskatsarakis@hotmail.com>
2023-11-02 00:20:13 +0200

    added abbility to add a new income

--- life-dashboard/src/main/frontend/expense-app/src/Components/IncomeComponent.tsx
+++ life-dashboard/src/main/frontend/expense-app/src/Components/IncomeComponent.tsx
@@ -1,25 +1,27 @@
-import {useParams} from "react-router-dom";
-import {FormEvent, useEffect, useState} from "react";
-import {samples} from "../Utilities/Samples";
-import {Col, Form, Row, InputGroup, Button} from "react-bootstrap";
-import axios, {AxiosResponse} from "axios";
-import {variables} from "../Utilities/Variables";
+import { useParams } from "react-router-dom";
+import { FormEvent, useEffect, useState } from "react";
+import { samples } from "../Utilities/Samples";
+import { Col, Form, Row, InputGroup, Button } from "react-bootstrap";
+import axios, { AxiosResponse } from "axios";
+import { variables } from "../Utilities/Variables";
 
 
 
 export const IncomeComponent = () => {
-    const {incomeId}  = useParams();
+    const { incomeId } = useParams();
     const [moneyReceived, setMoneyReceived] = useState(1);
-    const [description, setDescription]  = useState(null);
+    const [description, setDescription] = useState('');
     const today = new Date();
     const [dateCreate, setDateCreated] = useState(today);
+    const [incomeSource, setIncomeSource] = useState('')
     const [incomeSources, setIncomeSources] = useState([])
     useEffect(
         () => {
             // @ts-ignore
-            if(incomeId !== -1 ) {
+            if (incomeId !== -1) {
                 // @ts-ignore
-                const {incomeId, description, moneyReceived, dateCreated} = fetchIncomeById();
+                const { incomeId, description, moneyReceived, dateCreated } = fetchIncomeById();
+                console.log(incomeId, description, moneyReceived, dateCreated)
                 setDateCreated(dateCreated);
                 setMoneyReceived(moneyReceived);
             }
@@ -37,27 +39,24 @@ export const IncomeComponent = () => {
         return samples.SAMPLE_INCOMES;
     }
 
-    async function createIncome(income: { dateCreated: any; description: any; moneyReceived: number; incomeSource: any}) {
+    async function createIncome(income: { dateCreated: any; description: any; moneyReceived: number; }) {
         console.log(income);
-        const postIncome = await axios.post(variables.fetchIncomesURL, income);
+        const postIncome = await axios.post(`http://localhost:8080/api/v1/${incomeSource}/incomes`, income);
         console.log(postIncome.data);
     }
 
-    const handleSubmit  = (event : FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         if (dateCreate == null) setDateCreated(today);
         const income = {
             moneyReceived: moneyReceived,
             description: description,
             dateCreated: dateCreate,
-            incomeSource: {
-                'incomeSourceId': 10001
-            }
         }
-        if(moneyReceived > 0) {
+        if (moneyReceived > 0) {
             setMessage(`The money are ${moneyReceived}€`);
             createIncome(income);
-        }else {
+        } else {
             setMessage(" No money was set");
         }
 
@@ -65,9 +64,9 @@ export const IncomeComponent = () => {
     const [message, setMessage] = useState('');
     return (
         <>
-           <Form noValidate onSubmit={handleSubmit}>
-               <Row className='mb-3'>
-                   <Form.Group as={Col} md={'2'} >
+            <Form noValidate onSubmit={handleSubmit}>
+                <Row className='mb-3'>
+                    <Form.Group as={Col} md={'2'} >
                         <Form.Label>ID:</Form.Label>
                         <Form.Control
                             type={'text'}
@@ -75,54 +74,71 @@ export const IncomeComponent = () => {
                             disabled={true}
 
                         />
-                   </Form.Group>
-                   <Form.Group as={Col} md={'4'} >
-                       <Form.Label>Money Received</Form.Label>
-                       <InputGroup hasValidation>
-                           <InputGroup.Text id="inputGroupPrepend">€</InputGroup.Text>
-                           <Form.Control
-                               type="number"
-                               placeholder="Money Received"
-                               aria-describedby="inputGroupPrepend"
-                               required
-                               onChange={(event) => {setMoneyReceived(parseInt(event.target.value))}}
-                           />
+                    </Form.Group>
+                    <Form.Group as={Col} md={'4'} >
+                        <Form.Label>Money Received</Form.Label>
+                        <InputGroup hasValidation>
+                            <InputGroup.Text id="inputGroupPrepend">€</InputGroup.Text>
+                            <Form.Control
+                                type="number"
+                                placeholder="Money Received"
+                                aria-describedby="inputGroupPrepend"
+                                required
+                                value={moneyReceived}
+                                onChange={(event) => { setMoneyReceived(parseInt(event.target.value)) }}
+                            />
 
-                       </InputGroup>
-                   </Form.Group>
-                   <Form.Group as={Col} md={'4'} >
-                       <Form.Label>Date Received</Form.Label>
-                           <Form.Control
-                               type="date"
-                               placeholder="Date Received"
-                               defaultValue={today.toISOString().split('T')[0]}
-                               onChange={event => {setDateCreated(new Date(event.target.value.toString()))}}
-                               required
-                           />
+                        </InputGroup>
+                    </Form.Group>
+                    <Form.Group as={Col} md={'4'} >
+                        <Form.Label>Date Received</Form.Label>
+                        <Form.Control
+                            type="date"
+                            placeholder="Date Received"
+                            defaultValue={today.toISOString().split('T')[0]}
+                            onChange={event => { setDateCreated(new Date(event.target.value.toString())) }}
+                            required
+                        />
 
-                   </Form.Group>
-               </Row>
-               <Row className='mb-3'>
+                    </Form.Group>
+                </Row>
+                <Row className='mb-3'>
 
-                   <Form.Group as={Col} md={'4'} >
-                       <InputGroup>
-                           <Form.Select >
-                               <option selected>Select Income Source Type</option>
-                               {incomeSources.map(
-                                   source => {
-                                       //@ts-ignore
-                                       return ( <option>{source.incomeType}</option> )
-                                   }
-                               )}
-                           </Form.Select>
-                           <Button variant={'success'}>+</Button>
-                       </InputGroup>
+                    <Form.Group as={Col} md={'8'} >
+                        <InputGroup>
+                            <Form.Select
+                                onChange={(event) => { setIncomeSource(event.currentTarget.value.toString()) }}
+                            >
+                                <option selected>Select Income Source Type</option>
+                                {incomeSources.map(
+                                    source => {
+                                        //@ts-ignore
+                                        return (<option value={source.incomeSourceId}>{source.incomeType}</option>)
+                                    }
+                                )}
+                            </Form.Select>
+                            <Button variant={'success'}>+</Button>
+                        </InputGroup>
 
-                   </Form.Group>
-               </Row>
-               <Button type={'submit'}>Save</Button>
-               { message}
-           </Form>
+                        <Form.Label>Income Description</Form.Label>
+                        <InputGroup hasValidation>
+                            <InputGroup.Text id="Description">€</InputGroup.Text>
+                            <Form.Control
+                                type="text"
+                                placeholder="Description"
+                                aria-describedby="Description"
+                                value={description.toString()}
+                                required
+                                onChange={(event) => { setDescription(event.target.value.toString()) }}
+                            />
+
+                        </InputGroup>
+
+                    </Form.Group>
+                </Row>
+                <Button type={'submit'}>Save</Button>
+                {message}
+            </Form>
         </>
     );
 };

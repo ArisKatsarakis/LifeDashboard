1b07c8799b51d0a2fde230afa6f50b9162a5854c
ArisKatsarakis <ariskatsarakis@hotmail.com>
2023-12-14 22:59:50 +0200

    Added CommandLinner for adding Sample Incomes

--- life-dashboard/src/main/java/gr/ariskatsarakis/lifedashboard/LifeDashboardApplication.java
+++ life-dashboard/src/main/java/gr/ariskatsarakis/lifedashboard/LifeDashboardApplication.java
@@ -1,13 +1,37 @@
 package gr.ariskatsarakis.lifedashboard;
 
+import gr.ariskatsarakis.lifedashboard.income.def.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class LifeDashboardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LifeDashboardApplication.class, args);
+    }
+    @Bean
+    public CommandLineRunner demo(IncomeService incomeService, IncomeSourceService incomeSourceService) {
+        return (args) -> {
+            IncomeSource incomeSource = new IncomeSource();
+            incomeSource.setIncomeType(IncomeType.CODING);
+            incomeSource.setStabilityType(Stability.STABLE);
+            incomeSource.setName("CODING");
+            incomeSource = incomeSourceService.addIncomeSource(incomeSource);
+            Income income = new Income();
+            income.setIncomeSource(incomeSource);
+            income.setDescription("Income 1");
+            income.setMoneyReceived(BigDecimal.valueOf(100L));
+            income.setDateCreated(Timestamp.valueOf(LocalDateTime.now()));
+            incomeService.addIncome(income, incomeSource.getIncomeSourceId());
+
+        };
     }
 
 }

65bb70e22d54dfc120ecfeaedb84c80fec471bcd
ArisKatsarakis <ariskatsarakis@hotmail.com>
2024-02-27 21:34:16 +0200

    Add some expense and income samples

--- life-dashboard/backend/src/main/java/gr/ariskatsarakis/lifedashboard/budget/def/Entry.java
+++ life-dashboard/backend/src/main/java/gr/ariskatsarakis/lifedashboard/budget/def/Entry.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
-import java.time.LocalDate;
 
 @Entity
 @Table(name = "money_entries")
---
+++ life-dashboard/backend/src/test/java/gr/ariskatsarakis/lifedashboard/samples/IncomeExpenseSamples.java
@@ -0,0 +1,38 @@
+package gr.ariskatsarakis.lifedashboard.samples;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import gr.ariskatsarakis.lifedashboard.expense.beans.ExpenseType;
+import gr.ariskatsarakis.lifedashboard.expense.def.Expense;
+import gr.ariskatsarakis.lifedashboard.income.def.Income;
+import gr.ariskatsarakis.lifedashboard.income.def.IncomeSource;
+import gr.ariskatsarakis.lifedashboard.income.def.IncomeType;
+import jdk.jfr.snippets.Snippets.Example;
+
+public class IncomeExpenseSamples {
+
+	public Income getSampleIncome() {
+		Income income = new Income();
+		IncomeSource incomeSource = new IncomeSource();
+		incomeSource.setName("Name");
+		incomeSource.setIncomeType(IncomeType.CODING);
+		income.setIncomeId(1L);
+		income.setDateCreated(Timestamp.valueOf(LocalDateTime.now()));
+		income.setDescription("Sample Income");
+		income.setMoneyReceived(BigDecimal.TEN);
+		income.setIncomeSource(incomeSource);
+		return income;
+	}
+
+	public Expense getSampleExpense() {
+		Expense expense = new Expense();
+		expense.setExpenseId(1);
+		expense.setDateCreated(LocalDate.now());
+		expense.setExpenseType(ExpenseType.FUN);
+		expense.setMoneySpent(BigDecimal.TEN);
+		return expense;
+	}
+}

8bc5a3145bed6ed5c95d90ef8a689478ef87b6c0
ArisKatsarakis <ariskatsarakis@hotmail.com>
2024-05-17 20:13:28 +0300

    fixing a basic configuration

--- life-dashboard/backend/src/main/java/gr/ariskatsarakis/lifedashboard/budget/config/BasicConfiguration.java
+++ life-dashboard/backend/src/main/java/gr/ariskatsarakis/lifedashboard/budget/config/BasicConfiguration.java
@@ -2,30 +2,50 @@ package gr.ariskatsarakis.lifedashboard.budget.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-/**
- * TODO Make the fucking authentication
- */
 @Configuration
 public class BasicConfiguration {
-
+  /***
+   * Basic authentication configuratio
+   */
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.authorizeRequests(
-        req -> {
-          req.anyRequest().authenticated();
+    httpSecurity.authorizeHttpRequests(
+        auth -> {
+          auth.anyRequest().authenticated();
         });
+
     httpSecurity.sessionManagement(
-        ses -> {
-          ses.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        session -> {
+          session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         });
-    // httpSecurity.httpBasic(withDefaults());
-    System.out.println("getting through ");
-    httpSecurity.csrf(
-        csrf -> csrf.disable());
+    httpSecurity.httpBasic();
+    httpSecurity.csrf().disable();
     return httpSecurity.build();
   }
+
+  /**
+   * Users cereated here for inline memory
+   */
+  @Bean
+  public UserDetailsService getUserDetailsService() {
+    var user = User.withUsername("user").password("{noop}user").roles("USER").build();
+    var admin = User.withUsername("admin").password("{noop}user").roles("ADMIN").build();
+    return new InMemoryUserDetailsManager(user, admin);
+  }
+
+  /**
+   * Datasource configuration by datasource we connect the authentication to the
+   * db in order to save the user accounts there
+   */
+
 }
--- life-dashboard/backend/src/main/java/gr/ariskatsarakis/lifedashboard/budget/impl/BudgetController.java
+++ life-dashboard/backend/src/main/java/gr/ariskatsarakis/lifedashboard/budget/impl/BudgetController.java
@@ -1,13 +1,15 @@
 package gr.ariskatsarakis.lifedashboard.budget.impl;
 
-import gr.ariskatsarakis.lifedashboard.budget.beans.BudgetHistory;
-import gr.ariskatsarakis.lifedashboard.budget.def.Budget;
-import gr.ariskatsarakis.lifedashboard.budget.def.BudgetUtilities;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import gr.ariskatsarakis.lifedashboard.budget.beans.BudgetHistory;
+import gr.ariskatsarakis.lifedashboard.budget.def.Budget;
+import gr.ariskatsarakis.lifedashboard.budget.def.BudgetUtilities;
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * Controller for {@link Budget}
@@ -16,13 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BudgetController {
 
-    @Autowired
-    private BudgetUtilities budgetService;
+  @Autowired
+  private BudgetUtilities budgetService;
 
+  @GetMapping("api/v1/budget/history")
+  @CrossOrigin
+  public BudgetHistory budgetHistory() {
+    return budgetService.getBudgetHistory();
+  }
 
-    @GetMapping("api/v1/budget/history")
-    @CrossOrigin
-    public BudgetHistory budgetHistory() {
-        return  budgetService.getBudgetHistory();
-    }
+  @GetMapping("/csrf-token")
+  public CsrfToken getPublicToken(HttpServletRequest request) {
+    return (CsrfToken) request.getAttribute("_csrf");
+  }
+
 }

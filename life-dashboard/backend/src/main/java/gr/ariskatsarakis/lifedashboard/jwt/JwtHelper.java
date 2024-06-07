725ed0ac3c58370ee0e5c12deced49ed89ee03ee
ArisKatsarakis <ariskatsarakis@hotmail.com>
2024-06-08 01:43:23 +0300

    JwtHelper finished with all the utilities arount the Jwt Token filter and FilterChain pending to be developed

--- life-dashboard/backend/src/main/java/gr/ariskatsarakis/lifedashboard/jwt/JwtHelper.java
+++ life-dashboard/backend/src/main/java/gr/ariskatsarakis/lifedashboard/jwt/JwtHelper.java
@@ -1,6 +1,16 @@
 package gr.ariskatsarakis.lifedashboard.jwt;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;;
 
 /**
  * JwtHelper
@@ -10,4 +20,41 @@ public class JwtHelper {
   public static final long JWT_TOKEN_DURATION = 5 * 60 * 60;
   private String secret = "javainuse";
 
+  public String getUsernameFromToken(String token) {
+    return getClaimFromToken(token, Claims::getSubject);
+  }
+
+  public Date getExpirationDateFromToken(String token) {
+    return getClaimFromToken(token, Claims::getExpiration);
+  }
+
+  public <T> T getClaimFromToken(String token, Function<Claims, T> claimResolver) {
+    final Claims claims = getAllClaimsFromToken(token);
+    return claimResolver.apply(claims);
+  }
+
+  private Claims getAllClaimsFromToken(String token) {
+    return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+  }
+
+  public Boolean isTokenExpired(String token) {
+    Date date = getClaimFromToken(token, Claims::getExpiration);
+    return date.before(new Date());
+  }
+
+  public String generateToken(UserDetails userDetails) {
+    Map<String, Object> claims = new HashMap<>();
+    return doGenerateToken(claims, userDetails.getUsername());
+  }
+
+  public String doGenerateToken(Map<String, Object> claims, String subject) {
+    return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date())
+        .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_DURATION * 1000))
+        .signWith(SignatureAlgorithm.HS512, secret).compact();
+  }
+
+  public Boolean validateToken(String token, UserDetails userDetails) {
+    final String username = getUsernameFromToken(token);
+    return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+  }
 }

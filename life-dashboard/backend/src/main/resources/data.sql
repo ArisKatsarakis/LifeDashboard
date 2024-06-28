e6422d737146fcddb22001fc1321d1a60165f169
ArisKatsarakis <ariskatsarakis@hotmail.com>
2024-06-28 21:08:47 +0300

    added preset for DB adding Expenses and ExpenseTypes

---
+++ life-dashboard/backend/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO public.expense ( "money", "timestamp", expense_type_expense_type_id, expense, expense_type, "name") VALUES( 15.00, '2024-06-27 19:26:39.717', NULL, NULL, NULL, 'takakia');

37e44a5219958bfb08f73aea50035c1da76ebce9
ArisKatsarakis <ariskatsarakis@hotmail.com>
2024-07-08 02:25:59 +0300

    added Income Test at startup and a rule for adding one Income per month

--- life-dashboard/backend/src/main/java/gr/ariskatsarakis/lifedashboard/income/IncomeRepository.java
+++ life-dashboard/backend/src/main/java/gr/ariskatsarakis/lifedashboard/income/IncomeRepository.java
@@ -1,6 +1,9 @@
 package gr.ariskatsarakis.lifedashboard.income;
 
+import java.sql.Timestamp;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -8,5 +11,8 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface IncomeRepository extends JpaRepository<Income, Long> {
+
+  @Query("Select count(e.incomeId) from Income e where e.timestamp >= ?1 and e.timestamp <= ?2 ")
+  public Integer getIncomesForMonth(Timestamp start, Timestamp end);
 
 }
--- life-dashboard/backend/src/main/java/gr/ariskatsarakis/lifedashboard/income/IncomeService.java
+++ life-dashboard/backend/src/main/java/gr/ariskatsarakis/lifedashboard/income/IncomeService.java
@@ -2,8 +2,11 @@ package gr.ariskatsarakis.lifedashboard.income;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.Calendar;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,8 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class IncomeService {
+
+  private Logger incomeLogger = LoggerFactory.getLogger(IncomeService.class);
 
   @Autowired
   private IncomeRepository incomeRepository;
@@ -22,11 +27,22 @@ public class IncomeService {
 
   public Income addIncome(Income income) {
     income.setTimestamp(Timestamp.valueOf(LocalDateTime.now()));
-    checkIfIncomesExistForThisMonth(income);
+    if (checkIfIncomesExistForThisMonth(income) > 0) {
+      throw new RuntimeException("Too many Incomes this month");
+    }
+
     return incomeRepository.save(income);
   }
 
-  private void checkIfIncomesExistForThisMonth(Income income) {
-
+  private Integer checkIfIncomesExistForThisMonth(Income income) {
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(2024, 06, 01);
+    Timestamp start = new Timestamp(calendar.getTime().getTime());
+    calendar.set(2024, 06, 31);
+    Timestamp end = new Timestamp(calendar.getTime().getTime());
+    incomeLogger.info("START: " + start.toString());
+    incomeLogger.info("END: " + end.toString());
+    incomeLogger.info(incomeRepository.getIncomesForMonth(start, end).toString());
+    return incomeRepository.getIncomesForMonth(start, end);
   }
 }

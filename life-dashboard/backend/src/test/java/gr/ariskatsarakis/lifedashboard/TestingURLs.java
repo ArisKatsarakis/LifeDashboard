697045222c00d162f107fea5071bf7b313951738
ArisKatsarakis <ariskatsarakis@hotmail.com>
2024-12-07 18:50:12 +0200

    clean up java tests

--- life-dashboard/backend/src/test/java/gr/ariskatsarakis/lifedashboard/TestingURLs.java
+++ life-dashboard/backend/src/test/java/gr/ariskatsarakis/lifedashboard/TestingURLs.java
@@ -4,11 +4,5 @@ package gr.ariskatsarakis.lifedashboard;
  * TestingURLs
  */
 public class TestingURLs {
-  public static final String EXPENSES_URL = "/api/v1/expenses";
-  public static final String INCOMES_URL = "/api/v1/incomes";
-  public static final String EXPENSES_TYPES_URL = "/api/v1/expense-types";
-  public static final String WALLET_URL = "/api/v1/wallet";
-  public static final String LAST_WALLET_URL = "/api/v1/last-wallet";
   public static final String AUTH_URL = "/auth/login";
-  public static final String SAVING_GOAL_URL = "/api/v1/saving-goals";
 }
